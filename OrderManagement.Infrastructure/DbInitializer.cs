14917829b567bb65eb48b6b315d699ce2c6c5059
Martin Grulyó <106872157+mrtng1@users.noreply.github.com>
2025-05-09 12:59:19 +0200

    Postman API tests

--- OrderManagement.Infrastructure/DbInitializer.cs
+++ OrderManagement.Infrastructure/DbInitializer.cs
@@ -18,17 +18,18 @@ public class DbInitializer : IDbInitializer
         {
             context.Users.AddRange(
                 new User { Username = "Alice" },
-                new User { Username = "Bob" }
+                new User { Username = "Bob" },
+                new User { Id = Guid.Parse("0d8e48d3-d38a-421b-9f56-fae031222fc4") ,Username = "TestUser" }
             );
         }
 
         if (!context.Products.Any())
         {
             context.Products.AddRange(
-                //new Product { Id = Guid.Parse("086b496d-b01e-44af-9102-aa9c086ae1e4"), Name = "Bottles", Price = 10.99m },
-                //new Product { Id = Guid.Parse("6f129be7-ca5e-4796-8724-56429e30f40d"), Name = "Fries", Price = 4.99m },
                 new Product { Name = "Bottles", Price = 10.99m, Stock = 5},
-                new Product {  Name = "Fries", Price = 4.99m, Stock = 10}
+                new Product {  Name = "Fries", Price = 4.99m, Stock = 10},
+                new Product { Id = Guid.Parse("6f129be7-ca5e-4796-8724-56429e30f40d"), Name = "TestProduct", Price = 1.99m, Stock = 10},
+                new Product { Id = Guid.Parse("086b496d-b01e-44af-9102-aa9c086ae1e4"), Name = "TestProduct2", Price = 2.99m, Stock = 5}
             );
         }
 
---
+++ OrderManagement.IntegrationTests/ApiPostmanTests/readme.md
@@ -0,0 +1,6 @@
+# Integration Testing
+
+## Postman API tests
+
++ run the tests using newman <code> newman run {fileName} </code>
++ for example <code> newman run OrdersApiTests.json </code>
